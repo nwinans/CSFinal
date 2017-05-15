92c48e64e790fb1b9eb19636f5db2342a3f42bc9
Nick Winans <nwinans15@gmail.com>
2017-05-15 13:58:26 -0400

    added rope

---
+++ Rope.java
@@ -0,0 +1,11 @@
+public class Rope extends Item {
+	
+	public Rope(int x, int y) {	
+		super (x, y, 10, 20);
+	}
+	
+	public void grow() {
+		super.setHeight(getHeight() + 10);
+		super.incrementY(-10);
+	}
+}

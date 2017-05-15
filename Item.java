c2887d7bd7fea3fbc6005783ac530795e518c612
Nick Winans <nwinans15@gmail.com>
2017-05-15 13:57:06 -0400

    item is now class

--- Item.java
+++ Item.java
@@ -1,8 +1,50 @@
-public interface Item {
-	int getX();
-	int getY();
-	void setX(int x);
-	void setY(int y);
-	int getHeight();
-	int getWidth();
+public class Item {
+	private int x;
+	private int y;
+	
+	private int height;
+	private int width;
+	
+	public Item(int xCor, int yCor, int w, int h) {	
+		x = xCor;
+		y = yCor;
+		height = h;
+		width = w;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setX(int xCor) {
+		x = xCor;
+	}
+	
+	public void setY(int yCor) {
+		y = yCor;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public void incrementX(int a) {
+		x += a;
+	}
+	
+	public void incrementY(int a) {
+		y += a;
+	}
+	
+	public void setHeight(int h) {	
+		height = h;
+	}
 }

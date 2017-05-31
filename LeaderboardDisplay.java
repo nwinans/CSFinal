a1b764e43321276b53887b8b1c469762674b9354
nicholas winans <nwinans15@gmail.com>
2017-05-31 17:41:09 -0400

    Added Back Button
    Added back button to leaderboard page to return to main menu

--- LeaderboardDisplay.java
+++ LeaderboardDisplay.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.util.ArrayList;
 
 public class LeaderboardDisplay extends JPanel {
 	
 	private JFrame mainFrame;
+   
 	private Leaderboard leaderboard;
 	
    private final int SPACING = 40;
@@ -16,11 +18,26 @@ public class LeaderboardDisplay extends JPanel {
 		
 		//set the size of the frame to 400 width and 500 height
       mainFrame.setSize(400, 550);
+      setLayout(null);
+            //btn.setPreferredSize(new Dimension(100, 50));
+     // btn.setLocation((getWidth() - 100) / 2, 500);
+      
 		//make the frame visible (invisible by default)
       mainFrame.setVisible(true);
 		
 		mainFrame.add(this);
-		
+      mainFrame.setResizable(false);
+      JButton btn = new JButton("Back");
+      add(btn);
+      btn.setBounds(150, 450, 100, 50);
+      btn.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            mainFrame.dispose();
+            String[] args = {};
+            BallAttack.main(args);
+         }
+      });
+   
 		//close when the close button is pressed
       mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
